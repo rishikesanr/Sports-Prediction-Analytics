289003d67af890e8c6c09e4e0319f274623d741a
Rishikesan "Rishi" Ravichandran <61989009+rishikesanr@users.noreply.github.com>
2024-03-01 17:35:52 -0800

    Delete scraping/test.py

--- scraping/test.py
+++
@@ -1,8 +0,0 @@
-import praw
-reddit = praw.Reddit(user_agent='Mozilla/5.0',client_id='6YKv7QY_LYtgqL6x0VKgRw',client_secret='999dPOb7VY_4cW3-k0ilIjY11IMYxA',username='****',password='*****')
-
-url ="https://www.reddit.com/r/PremierLeague/comments/1b3plxw/liverpool_man_city_arsenal_who_will_win_the_2324/"
-
-post1 = reddit.submission(url=url)
-
-print(post1.selftext)
